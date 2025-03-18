596416edf1152c3fc95f0271b1fc687ada9c6ef0
MargaritaMuhina <130301530+MargaritaMuhina@users.noreply.github.com>
2025-03-19 02:15:43 +0300

    Мухина Маргарита. Задача 1. Вариант 21. Технология SEQ. Поиск кратчайших путей из одной вершины (алгоритм Дейкстры). (#68)
    Алгоритм Дейкстры используется для нахождения кратчайших путей от
    заданной начальной вершины до всех остальных вершин в графе.
    Создается вектор списков смежности adj_list, где каждый элемент
    adj_list[i] является вектором пар <dest, weight>, представляющих ребра,
    исходящие из вершины i.
    Преобразуются данные о графе из graph_data_ в представление списка
    смежности. Формат данных в graph_data_ предполагает последовательное
    хранение информации о ребрах. Значение -1 используется в graph_data_ как
    разделитель между списками ребер для разных вершин.
    Алгоритм Дейкстры:
    * Создается приоритетная очередь pq для хранения вершин, упорядоченных
    по текущему кратчайшему расстоянию до них. Используется std::greater<>
    для того, чтобы вершина с наименьшим расстоянием всегда находилась в
    начале очереди.
    * Добавляется начальная вершина start_vertex_ в приоритетную очередь с
    расстоянием 0.
    * Извлекается вершина u с наименьшим текущим расстоянием dist_u из
    приоритетной очереди.
    * Если извлеченное расстояние dist_u больше, чем текущее известное
    расстояние до вершины u ( distances_[u]), то это означает, что вершина u
    уже была обработана с более коротким путем, и текущая итерация
    пропускается.
    * Для каждого ребра, исходящего из вершины u (перебираются соседи v
    вершины u):
    * Вычисляется новое расстояние до соседней вершины v как distances_[u] +
    weight.
    * Если новое расстояние меньше текущего известного расстояния до вершины
    v ( distances_[v]), то обновляет distances_[v] и добавляется вершина v в
    приоритетную очередь с новым расстоянием

---
+++ tasks/seq/muhina_m_dijkstra/perf_tests/main.cpp
@@ -0,0 +1,123 @@
+#include <gtest/gtest.h>
+
+#include <chrono>
+#include <climits>
+#include <cstddef>
+#include <cstdint>
+#include <cstdlib>
+#include <memory>
+#include <utility>
+#include <vector>
+
+#include "core/perf/include/perf.hpp"
+#include "core/task/include/task.hpp"
+#include "seq/muhina_m_dijkstra/include/ops_seq.hpp"
+
+TEST(muhina_m_dijkstra_seq, test_pipeline_run) {
+  constexpr size_t kNumVertices = 6000;
+  size_t start_vertex = 0;
+
+  std::vector<std::vector<std::pair<size_t, int>>> adj_list(kNumVertices);
+  for (size_t i = 0; i < kNumVertices; ++i) {
+    for (size_t j = 0; j < kNumVertices; ++j) {
+      if (i != j) {
+        if (rand() % 3 == 0) {
+          int weight = (rand() % 10) + 1;
+          adj_list[i].emplace_back(j, weight);
+        }
+      }
+    }
+  }
+
+  std::vector<int> graph_data;
+  for (const auto& vertex_edges : adj_list) {
+    for (const auto& edge : vertex_edges) {
+      graph_data.push_back(static_cast<int>(edge.first));
+      graph_data.push_back(edge.second);
+    }
+    graph_data.push_back(-1);
+  }
+
+  std::vector<int> distances(kNumVertices, INT_MAX);
+
+  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(graph_data.data()));
+  task_data_seq->inputs_count.emplace_back(graph_data.size());
+
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(&start_vertex));
+  task_data_seq->inputs_count.emplace_back(sizeof(start_vertex));
+
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t*>(distances.data()));
+  task_data_seq->outputs_count.emplace_back(kNumVertices);
+
+  auto test_task_sequential = std::make_shared<muhina_m_dijkstra_seq::TestTaskSequential>(task_data_seq);
+
+  auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
+  perf_attr->num_running = 10;
+  const auto t0 = std::chrono::high_resolution_clock::now();
+  perf_attr->current_timer = [&] {
+    auto current_time_point = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time_point - t0).count();
+    return static_cast<double>(duration) * 1e-9;
+  };
+
+  auto perf_results = std::make_shared<ppc::core::PerfResults>();
+
+  auto perf_analyzer = std::make_shared<ppc::core::Perf>(test_task_sequential);
+  perf_analyzer->PipelineRun(perf_attr, perf_results);
+  ppc::core::Perf::PrintPerfStatistic(perf_results);
+}
+
+TEST(muhina_m_dijkstra_seq, test_task_run) {
+  constexpr size_t kNumVertices = 6000;
+  size_t start_vertex = 0;
+
+  std::vector<std::vector<std::pair<size_t, int>>> adj_list(kNumVertices);
+  for (size_t i = 0; i < kNumVertices; ++i) {
+    for (size_t j = 0; j < kNumVertices; ++j) {
+      if (i != j) {
+        if (rand() % 3 == 0) {
+          int weight = (rand() % 10) + 1;
+          adj_list[i].emplace_back(j, weight);
+        }
+      }
+    }
+  }
+
+  std::vector<int> graph_data;
+  for (const auto& vertex_edges : adj_list) {
+    for (const auto& edge : vertex_edges) {
+      graph_data.push_back(static_cast<int>(edge.first));
+      graph_data.push_back(edge.second);
+    }
+    graph_data.push_back(-1);
+  }
+
+  std::vector<int> distances(kNumVertices, INT_MAX);
+
+  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(graph_data.data()));
+  task_data_seq->inputs_count.emplace_back(graph_data.size());
+
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t*>(&start_vertex));
+  task_data_seq->inputs_count.emplace_back(sizeof(start_vertex));
+
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t*>(distances.data()));
+  task_data_seq->outputs_count.emplace_back(kNumVertices);
+
+  auto test_task_sequential = std::make_shared<muhina_m_dijkstra_seq::TestTaskSequential>(task_data_seq);
+
+  auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
+  perf_attr->num_running = 10;
+  const auto t0 = std::chrono::high_resolution_clock::now();
+  perf_attr->current_timer = [&] {
+    auto current_time_point = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time_point - t0).count();
+    return static_cast<double>(duration) * 1e-9;
+  };
+  auto perf_results = std::make_shared<ppc::core::PerfResults>();
+
+  auto perf_analyzer = std::make_shared<ppc::core::Perf>(test_task_sequential);
+  perf_analyzer->TaskRun(perf_attr, perf_results);
+  ppc::core::Perf::PrintPerfStatistic(perf_results);
+}
