68fe169577e253fc7afbe8508c95e033c372b678
Anton <132440944+lolipolovkovith@users.noreply.github.com>
2025-03-19 01:40:37 +0300

    Малышев Антон. Задача 1. Вариант 28. Технология SEQ. Повышение контраста полутонового изображения посредством линейной растяжки гистограммы. (#47)
    Алгоритм работает путем нахождения минимального и максимального значений
    яркости в изображении, а затем линейного масштабирования всех значений
    на полный диапазон от 0 до 255, что позволяет улучшить визуальное
    восприятие изображения за счет использования всего диапазона.
    
    Формула взята отсюда: [Обработка изображений. Часть 1, слайд
    7](http://www.graph.unn.ru/rus/materials/CG/CG03_ImageProcessing.pdf)

---
+++ tasks/seq/malyshev_a_increase_contrast_by_histogram/perf_tests/main.cpp
@@ -0,0 +1,88 @@
+#include <gtest/gtest.h>
+
+#include <chrono>
+#include <cstdint>
+#include <memory>
+#include <vector>
+
+#include "core/perf/include/perf.hpp"
+#include "core/task/include/task.hpp"
+#include "seq/malyshev_a_increase_contrast_by_histogram/include/ops_seq.hpp"
+
+TEST(malyshev_a_increase_contrast_by_histogram_seq, test_pipeline_run) {
+  constexpr int kCount = 150000000;
+
+  std::vector<int> in(kCount);
+  std::vector<int> out(kCount);
+
+  uint8_t tmp = 20;
+  for (int i = 0; i < kCount; i++) {
+    in[i] = tmp++;
+    if (tmp == 240) {
+      tmp = 20;
+    }
+  }
+
+  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+  task_data_seq->inputs_count.emplace_back(in.size());
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  task_data_seq->outputs_count.emplace_back(out.size());
+
+  auto test_task_sequential =
+      std::make_shared<malyshev_a_increase_contrast_by_histogram_seq::TestTaskSequential>(task_data_seq);
+
+  auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
+  perf_attr->num_running = 10;
+  const auto t0 = std::chrono::high_resolution_clock::now();
+  perf_attr->current_timer = [&] {
+    auto current_time_point = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time_point - t0).count();
+    return static_cast<double>(duration) * 1e-9;
+  };
+
+  auto perf_results = std::make_shared<ppc::core::PerfResults>();
+
+  auto perf_analyzer = std::make_shared<ppc::core::Perf>(test_task_sequential);
+  perf_analyzer->PipelineRun(perf_attr, perf_results);
+  ppc::core::Perf::PrintPerfStatistic(perf_results);
+}
+
+TEST(malyshev_a_increase_contrast_by_histogram_seq, test_task_run) {
+  constexpr int kCount = 150000000;
+
+  std::vector<int> in(kCount);
+  std::vector<int> out(kCount);
+
+  uint8_t tmp = 20;
+  for (int i = 0; i < kCount; i++) {
+    in[i] = tmp++;
+    if (tmp == 240) {
+      tmp = 20;
+    }
+  }
+
+  auto task_data_seq = std::make_shared<ppc::core::TaskData>();
+  task_data_seq->inputs.emplace_back(reinterpret_cast<uint8_t *>(in.data()));
+  task_data_seq->inputs_count.emplace_back(in.size());
+  task_data_seq->outputs.emplace_back(reinterpret_cast<uint8_t *>(out.data()));
+  task_data_seq->outputs_count.emplace_back(out.size());
+
+  auto test_task_sequential =
+      std::make_shared<malyshev_a_increase_contrast_by_histogram_seq::TestTaskSequential>(task_data_seq);
+
+  auto perf_attr = std::make_shared<ppc::core::PerfAttr>();
+  perf_attr->num_running = 10;
+  const auto t0 = std::chrono::high_resolution_clock::now();
+  perf_attr->current_timer = [&] {
+    auto current_time_point = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time_point - t0).count();
+    return static_cast<double>(duration) * 1e-9;
+  };
+
+  auto perf_results = std::make_shared<ppc::core::PerfResults>();
+
+  auto perf_analyzer = std::make_shared<ppc::core::Perf>(test_task_sequential);
+  perf_analyzer->TaskRun(perf_attr, perf_results);
+  ppc::core::Perf::PrintPerfStatistic(perf_results);
+}
