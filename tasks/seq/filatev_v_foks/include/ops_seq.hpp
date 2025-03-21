24ccbd8142927cc8efcf0b0a598ef6524bfc2e9a
Владислав <125855952+WladislawFilatew@users.noreply.github.com>
2025-03-21 15:59:23 +0300

    Филатьев Владислав. Технология SEQ. Умножение плотных матриц. Элементы типа double. Блочная схема, алгоритм Фокса. (#142)
    Этот код реализует алгоритм умножения матриц с использованием блочного
    метода (алгоритм Фокса). Код состоит из четырех основных методов:
    
    1) PreProcessingImpl():
    
    - Подготавливает данные: извлекает размеры матриц и блоков,
    инициализирует матрицы нулями и копирует входные данные в матрицы.
    
    2) ValidationImpl():
    
    - Проверяет корректность входных данных: размеры матриц должны быть
    согласованы, а размер блока — положительным.
    
    3) RunImpl():
    
    - Выполняет умножение матриц с использованием блочного метода. Результат
    сохраняется в результирующей матрице.
    
    4) PostProcessingImpl():
    
       - Копирует результат из результирующей матрицы в выходной массив.

---
+++ tasks/seq/filatev_v_foks/include/ops_seq.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <cstddef>
+#include <utility>
+#include <vector>
+
+#include "core/task/include/task.hpp"
+
+namespace filatev_v_foks_seq {
+
+struct MatrixSize {
+  size_t n;
+  size_t m;
+
+  MatrixSize() {
+    n = 0;
+    m = 0;
+  }
+  MatrixSize(size_t n, size_t m) : n(n), m(m) {}
+};
+
+class Focks : public ppc::core::Task {
+ public:
+  explicit Focks(ppc::core::TaskDataPtr task_data) : Task(std::move(task_data)) {}
+  bool PreProcessingImpl() override;
+  bool ValidationImpl() override;
+  bool RunImpl() override;
+  bool PostProcessingImpl() override;
+
+ private:
+  MatrixSize size_a_;
+  MatrixSize size_b_;
+  MatrixSize size_c_;
+
+  size_t size_block_{};
+  size_t size_{};
+
+  std::vector<double> matrix_a_;
+  std::vector<double> matrix_b_;
+  std::vector<double> matrix_c_;
+};
+
+}  // namespace filatev_v_foks_seq
