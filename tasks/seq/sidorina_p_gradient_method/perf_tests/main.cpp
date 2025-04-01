754adb34fc6bede5aa00025de8d69013fd0b0074
PolinaSidorina <113776414+PolinaSidorina@users.noreply.github.com>
2025-04-01 16:05:07 +0300

    FIX Сидорина Полина. Вариант 8. Технология SEQ. Решение систем линейных уравнений методом сопряженных градиентов. (#309)
    Код реализует метод сопряженных градиентов для решения линейной системы
    уравнений Ax = b, где A - матрица, x - вектор неизвестных, а b - вектор
    свободных членов. Важно, что матрица для использования этого метода
    обязательно должна быть симметрична и положительно определена (условие
    проверяется в validation).
    Начальное приближение решения solution задаётся (обычно нулевым
    вектором).
    residual: разность между правой частью (b) и результатом умножения
    матрицы на текущее приближение решения.
    Цикл итераций:
    Вычисляется норма квадрата остатка (residualNormSquared).
    Если норма остатка меньше заданной точности (tolerance), то метод
    завершается, возвращая текущее приближение решения.
    В противном случае вычисляется направление поиска (direction), которое
    является конъюгированным к предыдущим направлениям.
    matrixTimesDirection: результат умножения матрицы на вектор направления.
    Вычисляется коэффициент alpha, который используется для обновления
    решения.
    Обновляется solution и residual.
    Обновление направления: Вычисляется коэффициент beta и обновляется
    направление поиска (direction) для следующей итерации.

--- tasks/seq/sidorina_p_gradient_method/perf_tests/main.cpp
+++ tasks/seq/sidorina_p_gradient_method/perf_tests/main.cpp
@@ -12,7 +12,7 @@
 #include "seq/sidorina_p_gradient_method/include/ops_seq.hpp"
 
 TEST(sidorina_p_gradient_method_seq, test_pipeline_run) {
-  int size = static_cast<int>(std::pow(2, 10));
+  int size = static_cast<int>(std::pow(2, 12));
   std::vector<double> a(size * size);
   std::vector<double> a0(size * size);
   std::vector<double> b(size, 0);
@@ -23,7 +23,7 @@ TEST(sidorina_p_gradient_method_seq, test_pipeline_run) {
 
   std::random_device rd;
   std::mt19937 gen(rd());
-  std::uniform_real_distribution<double> dist(-10.0F, 10.0F);
+  std::uniform_real_distribution<double> dist(-1000.0F, 1000.0F);
 
   for (int i = 0; i < size; i++) {
     a0[i] = dist(gen);
@@ -89,7 +89,7 @@ TEST(sidorina_p_gradient_method_seq, test_task_run) {
 
   std::random_device rd;
   std::mt19937 gen(rd());
-  std::uniform_real_distribution<double> dist(-10.0F, 10.0F);
+  std::uniform_real_distribution<double> dist(-1000.0F, 1000.0F);
 
   for (int i = 0; i < size; i++) {
     a0[i] = dist(gen);
